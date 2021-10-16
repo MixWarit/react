0061c7d012414ec5b93400ccbc9902e779f6a45a
MixWarit <52699579+MixWarit@users.noreply.github.com>
2021-10-16 19:46:43 +0700

    Create Components
    แก้ไข Code ให้ใช้หลาย ๆ อย่างด้วย Components เป็นหลัก

--- App.js
+++ App.js
@@ -1,27 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
+import AppHeader from './components/AppHeader';
+import PicPost from './components/PicPost';
+import RosePic from './components/RosePic';
 function App() {
-  const textInput = <input type="text" />
-  const okButton = <button>ปุ่มกด</button>
   return (
     <div className="app">
-      <headers className="app-header">
-        <img className="app-header-logo"src="/images/test.png"/>
-      </headers>
+      <AppHeader />
       <div className="app-grid"> 
-          <div className="rose-pic">
-            <img src="/images/5.jpg"/>
-          </div>
-          <div className="rose-pic">
-            <img src="/images/1.jpg"/>
-          </div>
-          <div className="rose-pic">
-            <img src="/images/3.jpg"/>
-          </div>
-          <div className="rose-pic">
-            <img src="/images/2.jpg"/>
-          </div>
+        <RosePic />
+        <RosePic />
+        <RosePic />
+        <RosePic />
       </div>
     </div>
   );
